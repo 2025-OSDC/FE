bc52bd44c13c995d52fb7312cd138849c34de62a
hyeseong <tell280210@gmail.com>
2025-08-15 23:48:08 +0900

    ✨ feat: 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './layouts/Layout'
+import { Api, Install, Usage } from './pages'
+
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Install />} />
+        <Route path="api" element={<Api />} />
+        <Route path="usage" element={<Usage />} />
+      </Route>
+    </Routes>
   )
 }
 
