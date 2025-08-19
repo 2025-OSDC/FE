c84f9feb4f319c76e3e07c679d5a5dde4d1d9b34
김연진 <kyj0719@gmail.com>
2025-08-20 01:43:25 +0900

    ♻️refactor: 이름 변경 및 겹치는 파일 삭제

--- src/components/grayWord.tsx
+++
@@ -1,7 +0,0 @@
-export default function GrayWord({ word }: { word: string }) {
-  return (
-    <span className="bg-light-gray font-size-[23.04.px] w-fit rounded-[9.36px] px-[9.36px] py-[4.32px] font-normal text-black">
-      {word}
-    </span>
-  );
-}
