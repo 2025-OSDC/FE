ec732a040adf37e77195ecb59ee387392d304f67
jjjuni <0210leejun@gmail.com>
2025-08-20 04:15:45 +0900

    ♻️ refactor: 코드 리팩토링

---
+++ src/mocks/usage.ts
@@ -0,0 +1,39 @@
+export const COLORS = [
+  "#FF0000",
+  "#FF4000",
+  "#FF8000",
+  "#FFBF00",
+  "#FFFF00",
+  "#BFFF00",
+  "#7FFF00",
+  "#40FF00",
+  "#00FF00",
+  "#00FF40",
+  "#00FF7F",
+  "#00FFBF",
+  "#00FFFF",
+  "#00BFFF",
+  "#0080FF",
+  "#0040FF",
+  "#0000FF",
+  "#4000FF",
+  "#8000FF",
+  "#BF00FF",
+  "#FF00FF",
+  "#FF00BF",
+  "#FF007F",
+  "#FF0040",
+]
+
+export const TODOS = {
+  "시작 전": [
+    { title: "운동하기", content: "공복 유산소", number: 4 },
+    { title: "아침 먹기", content: "건강한 아침식사", number: 3 }
+  ],
+  "진행 중": [
+    { title: "프로젝트 작업", content: "코드 작성 및 리뷰", number: 2 }
+  ],
+  "완료": [
+    { title: "독서하기", content: "하루 한 챕터 읽기", number: 1 }
+  ]
+}
--- src/pages/Usage.tsx
+++ src/pages/Usage.tsx
@@ -2,36 +2,11 @@ import DonutChart from "../components/graph/DonutChart";
 import LineChart from "../components/graph/LineChart";
 import MapChart from "../components/graph/MapChart";
 import ProductChart from "../components/graph/ProductChart";
-import Todo from "../components/todo/Todo";
+import TodoList from "../components/todo/TodoList";
+import { COLORS, TODOS } from "../mocks/usage";
+import CustomTooltip from "../components/tooltip/Tooltip";
 
 export default function Usage() {
-
-  const COLORS = [
-    "#FF0000",
-    "#FF4000",
-    "#FF8000",
-    "#FFBF00",
-    "#FFFF00",
-    "#BFFF00",
-    "#7FFF00",
-    "#40FF00",
-    "#00FF00",
-    "#00FF40",
-    "#00FF7F",
-    "#00FFBF",
-    "#00FFFF",
-    "#00BFFF",
-    "#0080FF",
-    "#0040FF",
-    "#0000FF",
-    "#4000FF",
-    "#8000FF",
-    "#BF00FF",
-    "#FF00FF",
-    "#FF00BF",
-    "#FF007F",
-    "#FF0040",
-  ]
 
   return (
     <div className="text-center font-pretendard font-semibold">
@@ -46,7 +21,7 @@ export default function Usage() {
         {/* 그래프 */}
         <div className={`mt-[90px] flex flex-col gap-5`}>
           <p className={`text-[28px] w-full text-start`}>데이터 시각화</p>
-          <div className={`flex flex-col gap-10 pointer-events-none`}>
+          <div className={`flex flex-col gap-10`}>
             <div className={`flex flex-row gap-10`}>
               <div className={`w-[30%]`}>
                 <DonutChart />
@@ -68,10 +43,28 @@ export default function Usage() {
 
         {/* 상태 표시 / 컴포넌트 */}
         <div>
-          <p className={`text-[28px] w-full text-start`}>상태 표시 및 UI 컴포넌트</p>
-          <Todo state={"시작 전"} title={"운동하기"} content={"공복 유산소"} number={1}/>
+          <div className={`flex flex-col gap-6`}>
+            <p className={`text-[28px] w-full text-start`}>상태 표시 및 UI 컴포넌트</p>
+            <div className={`rounded-[28px] shadow-custom w-full px-6 py-10 flex flex-row gap-5`}>
+              <TodoList state={"시작 전"} todos={TODOS["시작 전"]} />
+              <TodoList state={"진행 중"} todos={TODOS["진행 중"]} />
+              <TodoList state={"완료"} todos={TODOS["완료"]} />
+
+            </div>
+          </div>
         </div>
 
+        {/* 툴팁 */}
+        <div className={`flex flex-col gap-10`}>
+          <div className={`flex flex-row gap-10`}>
+            <CustomTooltip state={"WARNING"} />
+            <CustomTooltip state={"INFO"} />
+          </div>
+          <div className={`flex flex-row gap-10`}>
+            <CustomTooltip state={"ERROR"} />
+            <CustomTooltip state={"SUCCESS"} />
+          </div>
+        </div>
       </div>
     </div>
   );
