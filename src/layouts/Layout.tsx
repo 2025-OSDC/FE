938bacb8e11a30c29691b27580bc74543683925a
jjjuni <0210leejun@gmail.com>
2025-08-20 00:35:05 +0900

    ✨ feat: colbrush 추가

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { ThemeProvider, ThemeSwitcher } from "colbrush";
 
 export default function Layout() {
   return (
     <div className="flex min-h-screen flex-col bg-white">
       <Header />
       <main className="flex-1">
-        <Outlet />
+        <ThemeProvider>
+          <Outlet />
+          <ThemeSwitcher className={`bottom-10 right-10`}/>
+        </ThemeProvider>
       </main>
       <Footer />
     </div>
