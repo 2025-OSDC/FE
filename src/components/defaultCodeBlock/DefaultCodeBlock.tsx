e8c2fa67cfd098ef93497fc482d1c22eb7c7d2d7
김연진 <kyj0719@gmail.com>
2025-08-20 01:51:05 +0900

    💄ui: padding 수정, 글자 색상 수정

--- src/components/defaultCodeBlock/DefaultCodeBlock.tsx
+++ src/components/defaultCodeBlock/DefaultCodeBlock.tsx
@@ -6,9 +6,11 @@ type TCodeBlockProps = {
 export default function CodeBlock({ code, language = "ts" }: TCodeBlockProps) {
   return (
     <pre
-      className={`w-full overflow-x-auto rounded-[32px] bg-gray-900 px-[44px] py-[33px] text-left font-mono text-sm leading-relaxed text-gray-100`}
+      className={`w-full overflow-x-auto rounded-[32px] bg-gray-900 px-[30.8px] py-[23.1px] text-left font-mono text-sm leading-relaxed`}
     >
-      <code className={`language-${language} block text-left whitespace-pre`}>
+      <code
+        className={`language-${language} text-bg block text-left whitespace-pre`}
+      >
         {code}
       </code>
     </pre>
