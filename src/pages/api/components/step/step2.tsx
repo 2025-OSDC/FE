fd1c14a9856df333ae09edc83d9ce8a99c44d14f
김연진 <kyj0719@gmail.com>
2025-08-20 03:42:35 +0900

    ✨feat: API 페이지 완성 및 codeblock 스타일링 수정

---
+++ src/pages/api/components/step/step2.tsx
@@ -0,0 +1,49 @@
+import Code from "../../../../components/code";
+import CodeBlock from "../../../../components/defaultCodeBlock/DefaultCodeBlock";
+import { ThemeProvider } from "../../constants";
+import Description from "../description";
+import SubTitle from "../subTitle";
+import Bullet from "../bullet";
+export default function Step2({ heading1 }: { heading1: string }) {
+  return (
+    <section className="flex flex-col gap-4">
+      <>
+        <h1 className={heading1}>2. 컴포넌트 (Components)</h1>
+        <Description>
+          이 섹션은 Colbrush가 제공하는 React 컴포넌트에 대한 상세 문서를 담고
+          있습니다.
+        </Description>
+      </>
+      <section>
+        <SubTitle>
+          <Code className="text-gray-blue text-[24px] font-medium">{`<ThemeProvider>`}</Code>
+        </SubTitle>
+        <Bullet option="설명: ">
+          애플리케이션에 테마 기능을 전역적으로 제공하는 Context Provider입니다.
+        </Bullet>
+        <Bullet option="Props: ">
+          <Code className="text-black">children</Code>
+          <span className="text-gray">(React 노드)</span>
+        </Bullet>
+        <Bullet option="사용 예시:"></Bullet>
+        <CodeBlock code={ThemeProvider}></CodeBlock>
+      </section>
+      <section>
+        <SubTitle>
+          <Code className="text-gray-blue text-[24px] font-medium">{`<ThemeSwitcher>`}</Code>
+        </SubTitle>
+        <Bullet option="설명: ">
+          사용자가 테마를 쉽게 변경할 수 있는 드롭다운 UI 컴포넌트입니다.
+        </Bullet>
+        <Bullet option="Props: ">
+          <Code className="text-black">Options</Code>
+          <span className="text-gray">
+            (선택 사항): 드롭다운에 표시될 테마 목록을 커스터마이징합니다.
+          </span>
+        </Bullet>
+        <Bullet option="사용 예시:"></Bullet>
+        <CodeBlock code={ThemeProvider}></CodeBlock>
+      </section>
+    </section>
+  );
+}
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,3 @@
-export { default as Api } from "./Api";
+export { default as Api } from "./api/Api";
 export { default as Install } from "./install";
 export { default as Usage } from "./Usage";
