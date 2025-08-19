da3590b1398f7c4fa5e1bcde254a04d303425bc6
jjjuni <0210leejun@gmail.com>
2025-08-20 04:13:28 +0900

    ✨ feat: Todo 컴포넌트 구현

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -19,16 +19,15 @@ const Todo: React.FC<TodoProps> = ({
   const [rootStyle, setRootStyle] = useState<CSSStyleDeclaration>(getComputedStyle(document.documentElement))
 
   const bgColor = state === "시작 전" ? `${rootStyle.getPropertyValue('--color-purple')}` :
-    state === "진행 중" ? `${rootStyle.getPropertyValue('--color-yellow')}` : `${rootStyle.getPropertyValue('--color-green')}`
+    state === "진행 중" ? `${rootStyle.getPropertyValue('--color-yellow')}` : `${rootStyle.getPropertyValue('--color-light-green')}`
 
   useEffect(() => {
     setRootStyle(getComputedStyle(document.documentElement))
   }, [theme])
 
-
   return (
-    <div className={`rounded-[16px] w-full px-5 py-4 flex flex-col items-start gap-2`}>
-      <div className={`text-[12px] py-1 px-2 w-fit rounded-[4px] bg-white`} style={{ backgroundColor: `${bgColor}33`, color: bgColor }}>
+    <div className={`rounded-[16px] w-full px-5 py-4 flex flex-col items-start gap-2 bg-white`}>
+      <div className={`text-[12px] py-0.5 px-1.5 w-fit rounded-[4px]`} style={{ backgroundColor: `${bgColor}33`, color: bgColor }}>
         {number}순위
       </div>
       <div className={`flex flex-col`}>
--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,15 +1,53 @@
+import { useTheme } from "colbrush";
+import { useEffect, useState } from "react";
+import Todo from "./Todo";
+
 type State = "시작 전" | "진행 중" | "완료";
 
-const TodoList = ({ state }: { state: State }) => {
+type Todo = {
+  title: string;
+  content: string;
+  number: number;
+}
+
+type TodoListProps = {
+  state: State;
+  todos: Todo[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({
+  state,
+  todos
+}) => {
+
+  const theme = useTheme().theme;
 
   const [rootStyle, setRootStyle] = useState<CSSStyleDeclaration>(getComputedStyle(document.documentElement))
 
+  useEffect(() => {
+    setRootStyle(getComputedStyle(document.documentElement));
+  }, [theme])
+
   const color = state === "시작 전" ? `${rootStyle.getPropertyValue('--color-purple')}` :
-    state === "진행 중" ? `${rootStyle.getPropertyValue('--color-yellow')}` : `${rootStyle.getPropertyValue('--color-green')}`
+    state === "진행 중" ? `${rootStyle.getPropertyValue('--color-yellow')}` : `${rootStyle.getPropertyValue('--color-light-green')}`
 
   return (
-    <div className={`rounded-[16px] bg-[#F5F5F5]`}>
-
+    <div className={`flex flex-col gap-4 rounded-[16px] bg-[#F5F5F5] w-full h-full p-4`}>
+      <div className={`flex flex-row gap-2.5 items-center`}>
+        <div className={`flex flex-row gap-2 items-center`}>
+          <div className={`w-2 h-2 rounded-full`} style={{ backgroundColor: color }} />
+          <p className={`text-start text-[16px]`}>{state}</p>
+        </div>
+        <div className={`w-5 h-5 bg-[#D8D8D8] rounded-full text-[12px] text-[#625F6D]`}>{todos.length}</div>
+      </div>
+      {todos.map((todo, index) => (
+          <Todo
+            key={index} 
+            state={state} 
+            title={todo.title} 
+            content={todo.content} 
+            number={todo.number}/>
+        ))}
     </div>
   )
 }
