9a13fd23927e93be65f5574f5f709b4a714b6109
hyeseong <tell280210@gmail.com>
2025-08-20 14:35:23 +0900

    💄 ui: 푸터 반응형 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,27 +3,35 @@ import { LogoText } from "./icons/LogoText";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#F0F0F0] px-28 py-6">
+    <footer className="bg-[#F0F0F0] px-4 py-4 sm:px-8 sm:py-5 lg:px-28 lg:py-6">
       <div className="mx-auto h-full w-full">
-        <div className="flex w-full items-end justify-between">
-          <div className="flex flex-col gap-6">
+        <div className="flex w-full flex-col gap-4 sm:flex-row sm:items-end sm:justify-between sm:gap-0">
+          <div className="flex flex-col gap-3 sm:gap-4 lg:gap-6">
             <div className="flex flex-col gap-1">
-              <p className="text-gray-blue text-sm font-normal">
+              <p className="text-gray-blue text-xs font-normal sm:text-sm">
                 Developed by Team ColorBrush
               </p>
-              <p className="text-gray-blue text-sm font-normal">
+              <p className="text-gray-blue text-xs font-normal sm:text-sm">
                 Contributors: 윤수호, 김연진, 윤혜성, 이준희, 노하영
               </p>
             </div>
 
-            <p className="text-gray-blue text-sm font-normal">
+            <p className="text-gray-blue text-xs font-normal sm:text-sm">
               Copyright© 2025. colbrush. All rights reserved.
             </p>
           </div>
 
-          <div className="flex items-end gap-3">
-            <Logo width={37} height={37} />
-            <LogoText width={132} height={40} />
+          <div className="flex items-end gap-2 sm:gap-3">
+            <Logo
+              width={28}
+              height={28}
+              className="sm:h-[32px] sm:w-[32px] lg:h-[37px] lg:w-[37px]"
+            />
+            <LogoText
+              width={100}
+              height={30}
+              className="sm:h-[35px] sm:w-[115px] lg:h-[40px] lg:w-[132px]"
+            />
           </div>
         </div>
       </div>
