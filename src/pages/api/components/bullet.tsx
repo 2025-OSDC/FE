a8b4fa207182de7c3e25fdf0d99433953ca20834
김연진 <kyj0719@gmail.com>
2025-08-20 23:53:43 +0900

    🐛fix: 잘못들어간 명령어와 스타일링 오류 수정

--- src/pages/api/components/bullet.tsx
+++ src/pages/api/components/bullet.tsx
@@ -7,7 +7,7 @@ export default function Bullet({
 }) {
   return (
     <div className={`mt-5 flex items-center gap-1.5 sm:gap-2 ${className}`}>
-      <div className="text-gray flex flex-row flex-wrap items-center gap-1 text-sm font-semibold sm:text-base lg:text-[18px]">
+      <div className="text-gray flex flex-row flex-wrap items-center gap-1 text-sm font-normal sm:text-base lg:text-[18px]">
         • {children}
       </div>
     </div>
